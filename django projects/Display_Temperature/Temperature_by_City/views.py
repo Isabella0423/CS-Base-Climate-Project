9c975325c075356c7bd28db024401cac4eb978b9
Isabella0423 <isayu2304@gmail.com>
2024-05-29 21:32:10 -0700

    imported pyowm stuff

--- django projects/Display_Temperature/Temperature_by_City/views.py
+++ django projects/Display_Temperature/Temperature_by_City/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from pyowm.owm import OWM
+#from pyowm.utils import config
+#from pyowm.utils import timestamps
+
 
 def home(request):
-    return HttpResponse("This is my project!")
+    owm = OWM('0aca60f9474adcfafed852266e956f71')
+    manager = owm.weather_manager()
+    #station_id = 39276
+    #historian = manager.station_tick_history(station_id, limit=4)
+    return HttpResponse("This is temperature: ", historian.temperature_series(unit="celsius"))
