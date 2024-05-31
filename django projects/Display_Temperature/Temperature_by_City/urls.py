158dafff9d6cedf08ece48069159dfd314ca0fa0
Isabella0423 <isayu2304@gmail.com>
2024-05-30 20:54:41 -0700

    added templates, html, etc

--- django projects/Display_Temperature/Temperature_by_City/urls.py
+++ django projects/Display_Temperature/Temperature_by_City/urls.py
@@ -2,5 +2,6 @@ from django.urls import URLPattern, path
 from Temperature_by_City import views
 
 urlpatterns = [
-    path("", views.home, name = "home")
+    path("", views.home, name = "home"),
+    path("AQI/<year>", views.annual_aqi, name="annual_aqi")
 ]
--- django projects/Display_Temperature/Temperature_by_City/views.py
+++ django projects/Display_Temperature/Temperature_by_City/views.py
@@ -1,54 +1,53 @@
+from xmlrpc.client import DateTime
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 from pyowm.owm import OWM
-#from pyowm.utils import config
-#from pyowm.utils import timestamps
+from django.shortcuts import render
+from datetime import datetime, timedelta
 
 
 def home(request):
     owm = OWM('0aca60f9474adcfafed852266e956f71')
     airpollution = owm.airpollution_manager()
-    start = 1606223802  # November 24, 2020
-    # ...or fetch history on a closed timeframe in the past
-    end = 1613864065  # February 20, 2021
+    start = datetime.strptime("2024/1/1", "%Y/%m/%d")
+    end = datetime.strptime("2024/5/30", "%Y/%m/%d")
     list_of_historical_values = airpollution.air_quality_history_at_coords(51.507351, -0.127758, start, end=end)  # London, GB
 
-    # Each item in the list_of_historical_values is an AirStatus object 
     n=0
     totalaqi=0
     for air_status in list_of_historical_values:
-        #air_status.co
-        #air_status.no
-        #air_status.no2
-        #air_status.o3
-        #air_status.so2
-        #air_status.pm2_5
-        #air_status.pm10
-        #air_status.nh3
-        print(air_status.aqi)  # air quality index
         totalaqi=totalaqi+air_status.aqi
         n=n+1
     
     avergeaqi=totalaqi/n
-    #mgr2=owm.weather_manager()
-    #manager = owm.weather_manager()
-    #station_id = 39276
-    #historian = manager.station_tick_history(station_id, limit=4)
     
-    place= "Toronto"
-    start_date= "1985-01-01"
-    end_date="1985-12-31"
-    #history = manager.weather_history_at_place(place, start_date, end_date)
-    #history_list = history.get_weathers()
-    #for weather in history_list:
-    #    temperature = weather.get_temperature('celsius') 
-    #   print(f'Temperature: {temperature}°C')
-        
 
+    return HttpResponse("Average AQI for 2024 so far:"+str(avergeaqi))
 
-
-    #print(historian.temperature_series(unit="celsius"))
-
-    return HttpResponse("Average AQI:"+str(avergeaqi))
+def annual_aqi(request, year):
+    owm = OWM('0aca60f9474adcfafed852266e956f71')
+    airpollution = owm.airpollution_manager()
+    t_start=str(year)+"/1/1"
+    t_end=str(year)+"/12/31"
+    print(t_start)
+    print(t_end)
+    start =datetime.strptime(t_start, "%Y/%m/%d")
+    end= datetime.strptime(t_end,"%Y/%m/%d")
+    list_of_historical_values = airpollution.air_quality_history_at_coords(51.507351, -0.127758, start, end=end)  # London, GB
+    n=0
+    totalaqi=0
+    for air_status in list_of_historical_values:
+        totalaqi=totalaqi+air_status.aqi
+        n=n+1
+    
+    averageaqi=totalaqi/n
+    
+    return render(
+        request,
+        'Temperature_by_City/aqi_template.html',
+        {
+            'year' : year,
+            'ave_aqi' : averageaqi
+        }
+    )
